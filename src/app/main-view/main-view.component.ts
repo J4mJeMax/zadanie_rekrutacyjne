259a3f63d76a2a58eb1fe4dfabf3642ee92c778b
j4mjemax <H20-wdoa123>
2023-07-02 16:12:57 +0200

    improved view

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -10,8 +10,13 @@ import {Product} from "../interfaces/product/product.interface";
   styleUrls: ['./main-view.component.scss']
 })
 export class MainViewComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   products: Product[] = [];
   orders: Product[] = [];
+  showDetails: any = false;
+  selectedProduct: any;
+  displayedColumns: string[] = ['name', 'date', 'price', 'client'];
+  dataSource = new MatTableDataSource<Product>(this.orders);
 
   constructor(private woocommerceService: WoocommerceService) {}
 
@@ -27,18 +32,17 @@ export class MainViewComponent {
     );
   }
 
+  openModal(element: any) {
+    console.log(element)
+    this.showDetails = true;
+    this.selectedProduct = element
+  }
+
+  closeModal() {
+    this.showDetails = false;
+  }
+
   ngOnInit() {
     this.getOrders();
   }
-
-  displayedColumns: string[] = ['name', 'date', 'price', 'client'];
-  dataSource = new MatTableDataSource<Product>(this.orders);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngAfterViewInit() {
-    //@ts-ignore
-    this.dataSource.paginator = this.paginator;
-  }
-
 }
